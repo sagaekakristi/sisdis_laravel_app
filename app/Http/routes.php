a54f0f0fb7e8ba0e0d18d5d9476855a77491ad9a
Saga Ekakristi <Saga Ekakristi>
2016-10-04 21:33:08 +0700

    Start load balancer node branch

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,60 +11,54 @@
 |
 */
 
-Route::get('/', function () {
-    return array(
-    	'code' => 1000,
-    	'message' => 'Sistem Terdistribusi - Adrianus Saga Ekakristi (saga.ekakristi@gmail.com)',
-    	'data' => null,
-	);
-});
+Route::get('/', 'Tugas5Controller@index');
 
-/*
- * General group
- */
-Route::group(['prefix'=>'general'], function()
-{
-    Route::get('/hello', 'GeneralController@hello');
-    Route::get('/uptime', 'GeneralController@uptime');
-    // Route::get('/demo', 'Tugas3Controller@demo');
-});
+// /*
+//  * General group
+//  */
+// Route::group(['prefix'=>'general'], function()
+// {
+//     Route::get('/hello', 'GeneralController@hello');
+//     Route::get('/uptime', 'GeneralController@uptime');
+//     // Route::get('/demo', 'Tugas3Controller@demo');
+// });
 
-/*
- * Tugas 3 group
- */
-Route::group(['prefix'=>'tugas3'], function()
-{
-    Route::get('/klien', 'Tugas3Controller@index');
-    Route::any('/server', 'Tugas3Controller@server');
-    Route::any('/client', 'Tugas3Controller@client');
+// /*
+//  * Tugas 3 group
+//  */
+// Route::group(['prefix'=>'tugas3'], function()
+// {
+//     Route::get('/klien', 'Tugas3Controller@index');
+//     Route::any('/server', 'Tugas3Controller@server');
+//     Route::any('/client', 'Tugas3Controller@client');
 
-    Route::get('/speksaya.wsdl', function() {
-        return File::get(public_path() . '/speksaya.wsdl');
-    });
+//     Route::get('/speksaya.wsdl', function() {
+//         return File::get(public_path() . '/speksaya.wsdl');
+//     });
     
-    // demo purpose only
-    Route::get('/demo', 'Tugas3Controller@demo');
-});
+//     // demo purpose only
+//     Route::get('/demo', 'Tugas3Controller@demo');
+// });
 
-/*
- * Tugas 4 group
- */
-Route::group(['prefix'=>'tugas4'], function()
-{
-    Route::get('/server/getImage/{filename}', 'Tugas4Controller@get_image_api');
-    Route::post('/server/postImage', 'Tugas4Controller@upload_image_api');
 
-    Route::get('/server/viewImage/{filename}', 'Tugas4Controller@view_image_direct'); // unused
-    Route::get('/klien/viewImage/{filename}', 'Tugas4Controller@view_image_hit');
+//  * Tugas 4 group
+ 
+// Route::group(['prefix'=>'tugas4'], function()
+// {
+//     Route::get('/server/getImage/{filename}', 'Tugas4Controller@get_image_api');
+//     Route::post('/server/postImage', 'Tugas4Controller@upload_image_api');
 
-    Route::get('/klien/uploadImage', 'Tugas4Controller@upload_image_ui');
-    Route::post('/klien/uploadImage/receiver', 'Tugas4Controller@upload_image_ui_receiver');
-});
+//     Route::get('/server/viewImage/{filename}', 'Tugas4Controller@view_image_direct'); // unused
+//     Route::get('/klien/viewImage/{filename}', 'Tugas4Controller@view_image_hit');
 
-/*
- * Tugas 5 group
- */
-Route::group(['prefix'=>'tugas5'], function()
-{
-    Route::get('/server', 'Tugas5Controller@index');
-});
+//     Route::get('/klien/uploadImage', 'Tugas4Controller@upload_image_ui');
+//     Route::post('/klien/uploadImage/receiver', 'Tugas4Controller@upload_image_ui_receiver');
+// });
+
+// /*
+//  * Tugas 5 group
+//  */
+// Route::group(['prefix'=>'tugas5'], function()
+// {
+//     Route::get('/server', 'Tugas5Controller@index');
+// });
