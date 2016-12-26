4295bf9d19c4111d2d1132cee50ec9a76780c033
Saga Ekakristi <Saga Ekakristi>
2016-12-26 12:43:33 +0700

    Swift done

--- app/Http/Controllers/CephConsumer.php
+++ app/Http/Controllers/CephConsumer.php
@@ -19,25 +19,29 @@ class CephConsumer extends Controller
     {
         //
         $guzzle_client = new GuzzleClient();
-        $url = 'grup1-01.sisdis.ui.ac.id:7480';
+        $url = 'grup1-01.sisdis.ui.ac.id:7480/auth/1.0';
         $response = $guzzle_client->request('GET', $url, [
             'headers' => array(
-                'Content-Type' => 'application/x-www-form-urlencoded',
-                'Host' => 'grup1-01.sisdis.ui.ac.id:7480',
-                'X-Amz-Date' => '20161226T040054Z',
-                'Authorization' => 'AWS4-HMAC-SHA256 Credential=UZHT321LEU84XA0KPV5R/20161226/us-east-1/execute-api/aws4_request, SignedHeaders=content-type;host;x-amz-date, Signature=619dd17d247062de009da2c455d3015f8330ad850babb4fe31fe90042cf837f7',
+                'X-Auth-User' => 'alvin:swift',
+                'X-Auth-Key' => 'Jwq9kFXmG0hsS0su1aILsfpLaH9yqjYLpJPALGy2',
+            ),
+        ]);
+        $xauthtoken = $response->getHeaderLine('X-Auth-Token');
+
+        //
+        $url = 'grup1-01.sisdis.ui.ac.id:7480/swift/v1/';
+        $response = $guzzle_client->request('GET', $url, [
+            'headers' => array(
+                'X-Auth-Token' => $xauthtoken,
             ),
         ]);
         $body_response = $response->getBody()->getContents();
+        $list_of_bucket = explode("\n", trim($body_response));
 
-        $parsed = simplexml_load_string($body_response);
-        foreach($parsed->Buckets as $bucket){
-            foreach($bucket as $inside_bucket){
-                $bucket_name = $inside_bucket->Name;
-                $url = url('ceph/' . $bucket_name);
-                $link = '<a href="' . $url . '">' . $bucket_name . '</a>' . '<br>';
-                echo $link;
-            }
+        foreach($list_of_bucket as $bucket_name){
+            $url = url('ceph/' . $bucket_name);
+            $link = '<a href="' . $url . '">' . $bucket_name . '</a>' . '<br>';
+            echo $link;
         }
     }
 
@@ -48,26 +52,29 @@ class CephConsumer extends Controller
     {
         //
         $guzzle_client = new GuzzleClient();
-        $url = 'grup1-01.sisdis.ui.ac.id:7480/'.$bucket_name;
+        $url = 'grup1-01.sisdis.ui.ac.id:7480/auth/1.0';
         $response = $guzzle_client->request('GET', $url, [
             'headers' => array(
-                'Content-Type' => 'application/x-www-form-urlencoded',
-                'Host' => 'grup1-01.sisdis.ui.ac.id:7480',
-                'X-Amz-Date' => '20161226T050931Z',
-                'Authorization' => 'AWS4-HMAC-SHA256 Credential=UZHT321LEU84XA0KPV5R/20161226/us-east-1/execute-api/aws4_request, SignedHeaders=content-type;host;x-amz-date, Signature=0605f1567aa7940a60f60bd585ca9d1d0f00ca6c14b4a9a6fc488bf7100c0ad4',
+                'X-Auth-User' => 'alvin:swift',
+                'X-Auth-Key' => 'Jwq9kFXmG0hsS0su1aILsfpLaH9yqjYLpJPALGy2',
+            ),
+        ]);
+        $xauthtoken = $response->getHeaderLine('X-Auth-Token');
+
+        //
+        $url = 'grup1-01.sisdis.ui.ac.id:7480/swift/v1/' . $bucket_name;
+        $response = $guzzle_client->request('GET', $url, [
+            'headers' => array(
+                'X-Auth-Token' => $xauthtoken,
             ),
         ]);
         $body_response = $response->getBody()->getContents();
+        $list_of_object = explode("\n", trim($body_response));
 
-        $parsed = simplexml_load_string($body_response);
-        dd($parsed->Contents);
-        foreach($parsed->Contents as $content){
-            foreach($content as $inside_content){
-                $content_key = $inside_content->Key;
-                $url = url('ceph/' . $bucket_name . '/' . $content_key);
-                $link = '<a href="' . $url . '">' . $content_key . '</a>' . '<br>';
-                echo $link;
-            }
+        foreach($list_of_object as $object_name){
+            $url = url('ceph/' . $bucket_name . '/' . $object_name);
+            $link = '<a href="' . $url . '">' . $object_name . '</a>' . '<br>';
+            echo $link;
         }
     }
 
@@ -77,5 +84,25 @@ class CephConsumer extends Controller
     public function get_file(Request $request, $bucket_name, $file_name)
     {
         //
+        $guzzle_client = new GuzzleClient();
+        $url = 'grup1-01.sisdis.ui.ac.id:7480/auth/1.0';
+        $response = $guzzle_client->request('GET', $url, [
+            'headers' => array(
+                'X-Auth-User' => 'alvin:swift',
+                'X-Auth-Key' => 'Jwq9kFXmG0hsS0su1aILsfpLaH9yqjYLpJPALGy2',
+            ),
+        ]);
+        $xauthtoken = $response->getHeaderLine('X-Auth-Token');
+
+        //
+        $url = 'grup1-01.sisdis.ui.ac.id:7480/swift/v1/' . $bucket_name . '/' . $file_name;
+        $response = $guzzle_client->request('GET', $url, [
+            'headers' => array(
+                'X-Auth-Token' => $xauthtoken,
+            ),
+        ]);
+        $body_response = $response->getBody()->getContents();
+
+        return $body_response;
     }
 }
